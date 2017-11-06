8a8a677bce0fb2f6d6ac3d8a96f5127df3f1da4d
max10rogerio <max10rogerio@hotmail.com>
2017-11-05 22:00:42 -0200

    Configurado os arquivos para correto funcionamento

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,15 +1,18 @@
 app.config(function($routeProvider) {
   $routeProvider
   .when("/", {
-    templateUrl : "views/site/home.html",
+    templateUrl: "views/site/home.html",
     controller: "HomeController"
   })
   .when("/contato", {
-    templateUrl : "views/site/pg2.html"
+    templateUrl: "views/site/pg2.html"
   })
   .when("/candidato/perfil",{
     templateUrl: "views/candidato/perfil.html",
     controller: 'PerfilController'
+  })
+  .when("/cadastro", {
+    templateUrl: 'views/site/cadastro.html'
   })
   .otherwise({ redirectTo: '/'});
 
--- services/alert.service.js
+++ services/alert.service.js
@@ -1,17 +1,54 @@
 app.service('AlertService', function(){
+  /*
+    Docs:
+    https://github.com/limonte/sweetalert2
+    https://limonte.github.io/sweetalert2/
+  */
   this.errorModal = function(message){
     message = message || 'Nenhuma mensagem de erro cadastrada';
-    swal ( "Ops! Algo deu errado...", message, "error" );
+    swal ({
+      title: "Ops! Algo deu errado...",
+      html: message,
+      type: "error"
+    }).catch(swal.noop);
   };
 
   this.successModal = function(message){
     message = message || 'Nenhuma mensagem de sucesso cadastrada';
-    swal ( "Tudo certo! :)" ,  message ,  "success" );
+    swal ( "Tudo certo! :)" , message,  "success" );
+    swal ({
+      title: "Tudo certo! :)",
+      html: message,
+      type: "success"
+    }).catch(swal.noop);
   };
 
-  this.warningModal = function(message){
-    message = message || 'Nenhuma mensagem de aviso cadastrado...';
-    swal ( "Cuidado! o.O", message,  "warning" );
+  /*
+    Caso não consiga obter o resultado com as outras modals, utilize esta função...
+    Tipos de modals:
+      - warning
+      - error
+      - success
+      - info
+      - question
+    Parametros:
+      - type: Recebe o tipo da modals
+      - title: titulo da modal
+      - html: Recebe a descricao da modal, pode ser passado um html
+  */
+  this.showModal = function(type, title, html){
+    type = type || 'error';
+    title = title || 'Ops! Algo deu errado...';
+    html = html || '<u>Nenhuma descricao cadastrada</u>';
+    swal({
+      title: title,
+      type: type,
+      html: html
+    }).catch(swal.noop); //swal.noop retira erro de promise do log do navegador
   };
 
+  this.loadingModal = function(){
+    swal({title: 'Aguarde...'});
+    swal.showLoading();
+  };
 });
