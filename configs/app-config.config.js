b9fb59820208ae2d022fbc3199fa3ccb9b272775
max10rogerio <max10rogerio@hotmail.com>
2017-11-08 20:53:58 -0200

    adicionado link de localhost ao arquivo config do angularjs

--- configs/app-config.config.js
+++ configs/app-config.config.js
@@ -1,5 +1,6 @@
 var app = angular.module('easyjobWeb', ['ngRoute', 'ngAnimate']);
 app.constant('config',{
   AppName: "EasyJob",
-  urlWebService: 'http://easyjob.luingry.com.br/web-service/'
+  urlWebService: 'http://easyjob.luingry.com.br/api/',
+  urlLocalHost: 'http://localhost/easyjob/api/'
 });
